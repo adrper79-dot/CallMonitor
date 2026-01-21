db61e7a1370bd7e02d193afd1d57aa988287584d
adrper79-dot <adrper79@gmail.com>
2026-01-21 01:17:14 -0500

    chore: untrack migrations and cleanup root

--- migrations/2026-01-17-survey-question-types.sql
+++
@@ -1,6 +0,0 @@
--- Add survey question type metadata to voice_configs
-ALTER TABLE public.voice_configs
-  ADD COLUMN IF NOT EXISTS survey_question_types jsonb DEFAULT '[]';
-
-COMMENT ON COLUMN public.voice_configs.survey_question_types IS
-  'Question type metadata: [{index: 0, type: "scale_1_5"}, {index: 1, type: "yes_no"}]';
